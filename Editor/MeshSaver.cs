9657e2dcc967c0f1da126f7b57d181c10abbef85
JennaScvl <myraloveless@gmail.com>
2023-11-09 19:49:06 -0500

    Create MeshSaver.cs

---
+++ Editor/MeshSaver.cs
@@ -0,0 +1,75 @@
+//Copyright 2023 JennaScvl on GitHub
+//https://github.com/JennaScvl/BnuuyTools
+//License: You may use and modify this script for your personal projects
+//However you may not distribute it, nor any piece of it.
+//If you wish to share the script please link to this github
+//If you wish to contribute improvements, feel free to do a pull request.
+
+using UnityEngine;
+using UnityEditor;
+
+public class MeshSaver : EditorWindow
+{
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    public string meshName = "SavedMesh";
+
+    [MenuItem("Bnuuy/Mesh Saver")]
+    public static void ShowWindow()
+    {
+        GetWindow<MeshSaver>("Mesh Saver");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Save Skinned Mesh as Asset", EditorStyles.boldLabel);
+
+        skinnedMeshRenderer = (SkinnedMeshRenderer)EditorGUILayout.ObjectField("Skinned Mesh Renderer", skinnedMeshRenderer, typeof(SkinnedMeshRenderer), true);
+
+        meshName = EditorGUILayout.TextField("Mesh Name:", meshName);
+
+        if (GUILayout.Button("Save Mesh"))
+        {
+            SaveMesh();
+        }
+    }
+
+    private void SaveMesh()
+    {
+        if (skinnedMeshRenderer == null)
+        {
+            Debug.LogError("Skinned Mesh Renderer must be assigned.");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(meshName))
+        {
+            Debug.LogError("Asset Name must be provided.");
+            return;
+        }
+
+        Mesh skinnedMesh = skinnedMeshRenderer.sharedMesh;
+
+        if (skinnedMesh == null)
+        {
+            Debug.LogError("No mesh found in Skinned Mesh Renderer.");
+            return;
+        }
+
+        // Clone the original mesh
+        Mesh clonedMesh = Object.Instantiate(skinnedMesh);
+
+        // Give it a name to show up in the Assets folder
+        clonedMesh.name = meshName;
+
+        // Save the cloned mesh as a new asset
+        string assetPath = "Assets/" + meshName + ".asset";
+        AssetDatabase.CreateAsset(clonedMesh, assetPath);
+        AssetDatabase.SaveAssets(); // Save and refresh the asset database
+
+        // Reassign the saved mesh asset to the skinned mesh renderer
+        skinnedMeshRenderer.sharedMesh = AssetDatabase.LoadAssetAtPath<Mesh>(assetPath);
+
+        Debug.Log("Skinned Mesh saved and applied successfully to " + assetPath);
+    }
+
+}
